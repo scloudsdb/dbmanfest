efb2c6df9e0a17456fe40b444fa38d35edcb5fd0
scloudsdb <steamcloudsdta@gmail.com>
2025-10-31 16:47:22 +0700

    Steamclouds Uploader (batch #1, 2 files) at 2025-10-31 16:47:22

---
+++ 965230.lua
@@ -0,0 +1,12 @@
+-- 965230's Lua and Manifest Created by Morrenus
+-- DORAEMON  STORY OF SEASONS
+-- Created: September 29, 2025 at 08:22:20 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(965230) -- DORAEMON  STORY OF SEASONS
+-- MAIN APP DEPOTS
+addappid(965231, 1, "7991997f37bd388822b50e2b9e2984c14593fd80b32bfbc60baa0e795b037c04") -- An3TTDSK Content
+setManifestid(965231, "5336583278541251712", 822681663)
