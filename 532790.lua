cefd03105f4b5cde6e6484ac4088f6477103cea1
scloudsdb <steamcloudsdta@gmail.com>
2025-10-31 16:46:27 +0700

    Steamclouds Uploader (batch #1, 7 files) at 2025-10-31 16:46:26

---
+++ 532790.lua
@@ -0,0 +1,33 @@
+-- 532790's Lua and Manifest Created by Morrenus
+-- Vampire: The Masquerade - Bloodlines 2
+-- Created: October 21, 2025 at 12:06:00 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 6
+-- Total DLCs: 3 (3 excluded)
+-- Shared Depots: 5
+
+-- MAIN APPLICATION
+addappid(532790) -- Vampire: The Masquerade - Bloodlines 2
+-- MAIN APP DEPOTS
+addappid(532791, 1, "5031e75920df33014e680cad671ff0895f9e439d33eedb5b5878aa407d5cb177") -- Depot 532791
+setManifestid(532791, "4773644022543190207", 26569665426)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229004, 1, "56ebe05d052f59ad2e9b101102de603dca17402a9bb2ef0015962c6b3275a2d0") -- .NET 4.5.2 Redist (Shared from App 228980)
+setManifestid(229004, "5220958916987797232", 70000464)
+addappid(229005, 1, "aa18a527cdc103aeb3d12caafd6f581689a14467aa55658ba583a9af6e90313a") -- .NET 4.6 Redist (Shared from App 228980)
+setManifestid(229005, "7992454656023763365", 62009092)
+addappid(229033, 1, "9a789440bbc16131547ac6c062e5f168f7684da533a2899a71fdfaaeec21efc3") -- PhysX System Software 9.14.0702 (Shared from App 228980)
+setManifestid(229033, "2059065101492814639", 60039803)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1048510) -- Vampire The Masquerade - Bloodlines 2 - Bloodlines Nostalgia Jukebox
+addappid(1048511) -- Vampire The Masquerade - Bloodlines 2 - Santa Monica Memories
+addappid(3992600) -- Vampire The Masquerade - Bloodlines 2 - Expansion Pass
+-- EXCLUDED DLCS:
+-- UNRELEASED DLCS (COMMENTED OUT)
+-- addappid(1048512) -- Vampire: The Masquerade® - Bloodlines™ 2 - Shadows & Silk (unreleased)
+-- addappid(3996800) -- Vampire: The Masquerade® - Bloodlines™ 2 -  Loose Cannon  (unreleased)
+-- addappid(3996820) -- Vampire: The Masquerade® - Bloodlines™ 2 - The Flower & the Flame (unreleased)
