33ff3480ec38335d25251914219c488f17eedaa5
scloudsdb <steamcloudsdta@gmail.com>
2025-10-31 16:39:37 +0700

    Steamclouds Uploader (batch #1, 11 files) at 2025-10-31 16:39:36

---
+++ 1984270.lua
@@ -0,0 +1,60 @@
+-- 1984270's Lua and Manifest Created by Morrenus
+-- Digimon Story Time Stranger
+-- Created: October 03, 2025 at 03:36:40 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 10
+-- Total DLCs: 8 (5 excluded)
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(1984270) -- Digimon Story Time Stranger
+-- MAIN APP DEPOTS
+addappid(1984271, 1, "b0aa8ddfef2996e3122b0bda1d02f7ceb28f608ac8c1d853e6793790aaab8860") -- Depot 1984271
+setManifestid(1984271, "238061488930819289", 29885980165)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+-- DLCS WITH DEDICATED DEPOTS
+-- Digimon Story Time Stranger - Pre-Order Bonus Pack (AppID: 3524240)
+addappid(3524240)
+addappid(3524240, 1, "f06dc8a525a2fb5e03453da570b80915bd7c02daae714ac854c86283a2bc4e18") -- Digimon Story Time Stranger - Pre-Order Bonus Pack - Depot 3524240
+setManifestid(3524240, "6662609926555286405", 44603435)
+-- Digimon Story Time Stranger - Costume Swimwear Set (AppID: 3524320)
+addappid(3524320)
+addappid(3524320, 1, "0da086161abea2f6ce0863a530fefc41ac12993fb4308ff16301926398803a27") -- Digimon Story Time Stranger - Costume Swimwear Set - Depot 3524320
+setManifestid(3524320, "2937343108483999987", 4902)
+-- Digimon Story Time Stranger - Costume Chosen Children Set (AppID: 3524330)
+addappid(3524330)
+addappid(3524330, 1, "2358d5ff200cefb1eef6f77277f04089a01daaea4418cd48d530b4de3fe53f61") -- Digimon Story Time Stranger - Costume Chosen Children Set - Depot 3524330
+setManifestid(3524330, "9011286650463550421", 2826)
+-- Digimon Story Time Stranger - Costume Digimon Costume Set (AppID: 3524340)
+addappid(3524340)
+addappid(3524340, 1, "5441f08ccf7ebad6314bac9d071ba47a41e709f4000e121004752959138bb2dc") -- Digimon Story Time Stranger - Costume Digimon Costume Set - Depot 3524340
+setManifestid(3524340, "9171927283280801773", 1003)
+-- Digimon Story Time Stranger -  Costume Public Safety Suit  Special Supplies Set (AppID: 3524350)
+addappid(3524350)
+addappid(3524350, 1, "388eb5d2862631b327cd83e312bc69e3f14ecf6087a6a2ec0860f20df764139d") -- Digimon Story Time Stranger -  Costume Public Safety Suit  Special Supplies Set - Depot 3524350
+setManifestid(3524350, "3574787827120316187", 303792)
+-- Digimon Story Time Stranger - Cyber Sleuth BGM Pack (AppID: 3524360)
+addappid(3524360)
+addappid(3524360, 1, "9ac71aa654d8726aaf514236b138ac5c075854dd8f33abbfeb1b4316302c7f7b") -- Digimon Story Time Stranger - Cyber Sleuth BGM Pack - Depot 3524360
+setManifestid(3524360, "9075694746551360982", 361378824)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(3524390) -- Digimon Story Time Stranger - Season Pass
+addappid(3524400) -- Digimon Story Time Stranger - Costume Pack
+-- EXCLUDED DLCS:
+-- DLCS EXCLUDED (MISSING DEPOT KEYS)
+-- Digimon Story Time Stranger - Farm Item Golden Moai (AppID: 3524250) - missing depot keys
+-- addappid(3524250)
+-- Digimon Story Time Stranger - Costume Cyber Sleuth Set (AppID: 3524300) - missing depot keys
+-- addappid(3524300)
+-- Digimon Story Time Stranger - Early Unlock Special Agumon  Gabumon (AppID: 3524310) - missing depot keys
+-- addappid(3524310)
+-- Digimon Story Time Stranger - Digimon Anime Song Pack (AppID: 3524370) - missing depot keys
+-- addappid(3524370)
+-- Digimon Story Time Stranger - Outer Dungeons The Halls of EXP, Gold, Materials (AppID: 3524380) - missing depot keys
+-- addappid(3524380)
