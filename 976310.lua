ae024458636a34cf394cf08df5f658d724bd0fb3
scloudsdb <steamcloudsdta@gmail.com>
2025-10-31 16:47:31 +0700

    Steamclouds Uploader (batch #1, 7 files) at 2025-10-31 16:47:31

---
+++ 976310.lua
@@ -0,0 +1,97 @@
+-- 976310's Lua and Manifest Created by Morrenus
+-- Mortal Kombat 11
+-- Created: September 30, 2025 at 01:48:39 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 6
+-- Total DLCs: 45
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(976310, 1, "b55a8bb9b15f963446c6555e564422e54ea199cd933c33ea19461c0ccf3ea34b") -- Mortal Kombat 11
+-- MAIN APP DEPOTS
+addappid(976311, 1, "ba51ae236d415e34a5aa7366e15b74cd96587031f70d2acaf46ac5b390b0f4d9") -- Seven11 [codename] Content
+setManifestid(976311, "6512752024476768865", 117250155467)
+addappid(976312, 1, "37130205b90f533304b81a7d155c3f74576759eaab5e9f7d4e92478404205f60") -- SEVEN11_Binaries
+setManifestid(976312, "2372612785967955744", 351982190)
+addappid(976313, 1, "b8e08c7b5961c5daa928f561f46d5c8438ca9c016ed0c5e9bbe5827d0b7dfdb7") -- SEVEN11_Endpoints
+setManifestid(976313, "4245947103270175452", 92)
+-- SHARED DEPOTS (from other apps)
+addappid(228986, 1, "51dd3611d28621644730736f3bb1fd6b960053a45cd79123f2b9a80c9181dad5") -- VC 2015 Redist (Shared from App 228980)
+setManifestid(228986, "8782296191957114623", 29759921)
+-- DLCS WITH DEDICATED DEPOTS
+-- Aftermath Story (AppID: 1316183)
+addappid(1316183)
+addtoken(1316183, "17258017834634481351")
+addappid(1316183, 1, "ad7b646a99d28411d65e8f72de9a641edd4c778f7bfb98681eb63f2e0e19cfb1") -- Aftermath Story - Aftermath Story (1316183) Depot
+setManifestid(1316183, "2020495199252320891", 7490409356)
+-- 4K Cinematic Pack  (AppID: 1461160)
+addappid(1461160)
+addappid(1461160, 1, "d8a15e3cfcd6d44dd3cb30fb1477f61b3b5e6f5cdbf54c2148e2aab61d609ba1") -- 4K Cinematic Pack  - 4k Cinematics (1461160) Depot
+setManifestid(1461160, "5735050710340606220", 39525866392)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(1051620) -- Shao Kahn
+addappid(1051621) -- Frost
+addappid(1054480) -- Kombat Pack1
+addappid(1054870) -- Kold War Skarlet
+addappid(1054871) -- Cangaceiro Kano
+addappid(1054872) -- Gold Demon Scorpion
+addappid(1054873) -- Ninja Mime Johnny Cage
+addappid(1095030) -- Shang Tsung
+addappid(1095031) -- Klassic Ninja Skins
+addappid(1102090) -- Shang Tsung Movie Skins
+addappid(1116620) -- Nightwolf
+addtoken(1116620, "12493272622588703308")
+addappid(1116621) -- Klassic Arcade Fighter Pack
+addtoken(1116621, "14663220391896737013")
+addappid(1116622) -- War Games Sonya Blade
+addtoken(1116622, "15842350001407264177")
+addappid(1160240) -- Terminator T-800
+addtoken(1160240, "17128396078543704387")
+addappid(1160241) -- Double Feature Skin Pack
+addtoken(1160241, "17223401320969048271")
+addappid(1160242) -- Cassie Quinn
+addtoken(1160242, "11207879261656686706")
+addappid(1160243) -- Masquerade Skin Pack
+addappid(1180720) -- Sindel
+addtoken(1180720, "7803015090967595566")
+addappid(1180721) -- Gothic Horror Skin Pack
+addtoken(1180721, "5781167843207166001")
+addappid(1180722) -- Klassic Cassie
+addtoken(1180722, "3557708058148800682")
+addappid(1200550) -- The Joker
+addappid(1200551) -- Time Lord of Apokolips Geras
+addappid(1200552) -- DC Elseworlds Skin Pack
+addappid(1253300) -- Spawn
+addtoken(1253300, "15979675729663044800")
+addappid(1253301) -- Hellspawn Jacqui
+addtoken(1253301, "7313392218119549452")
+addappid(1253302) -- Matinee Skin Pack
+addtoken(1253302, "7121575206995700928")
+addappid(1273970) -- Mortal Kombat11 Aftermath  Kombat Pack Bundle
+addappid(1273971) -- Mortal Kombat11 Aftermath Expansion
+addappid(1316180) -- Fujin
+addtoken(1316180, "7385712625288293082")
+addappid(1316181) -- Sheeva
+addtoken(1316181, "8505132378874468315")
+addappid(1316182) -- Robocop
+addtoken(1316182, "12221699817818724261")
+addappid(1316184) -- Eternal Klash Skin Pack
+addappid(1316185) -- Thanks A Million Johnny Cage
+addtoken(1316185, "9749570368331811933")
+addappid(1375580) -- Summer Heat Skin Pack
+addtoken(1375580, "17246012852030160350")
+addappid(1396460) -- Klassic Femme Fatale Skin Pack
+addtoken(1396460, "17334866501163965620")
+addappid(1415130) -- All Hallows Eve Skin Pack
+addtoken(1415130, "9108617708585246192")
+addappid(1449880) -- Kombat Pack 2
+addappid(1449881) -- Rain
+addtoken(1449881, "10025843513073481141")
+addappid(1449882) -- Mileena
+addtoken(1449882, "8738007944097354560")
+addappid(1449883) -- Rambo
+addtoken(1449883, "12180670094604275504")
+addappid(1449890) -- Time Warriors Skin Pack
+addtoken(1449890, "5779223872582353032")
+addappid(1471870) -- Ultimate Add-On Bundle
+addappid(1480230) -- Klassic MK Movie Skin Pack
