812cd9a9a3fffb3d6349c3e285ab67d7e8450489
scloudsdb <steamcloudsdta@gmail.com>
2025-10-31 16:43:17 +0700

    Steamclouds Uploader (batch #1, 2 files) at 2025-10-31 16:43:16

---
+++ 3616550.lua
@@ -0,0 +1,12 @@
+-- 3616550's Lua and Manifest Created by Morrenus
+-- Car Dealership Simulator 2
+-- Created: October 18, 2025 at 13:20:19 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(3616550) -- Car Dealership Simulator 2
+-- MAIN APP DEPOTS
+addappid(3616551, 1, "321f51821758384501bec04ad6a82ad3bab317a8de1b00971de7161775883c48") -- Depot 3616551
+setManifestid(3616551, "4590370267599091086", 5315799193)
