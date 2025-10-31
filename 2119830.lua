f3decfc696d275419f5e937dd55518ace7af4514
scloudsdb <steamcloudsdta@gmail.com>
2025-10-31 16:39:48 +0700

    Steamclouds Uploader (batch #1, 5 files) at 2025-10-31 16:39:47

---
+++ 2119830.lua
@@ -0,0 +1,22 @@
+-- 2119830's Lua and Manifest Created by Morrenus
+-- MISERY
+-- Created: October 27, 2025 at 08:57:51 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 4
+-- Total DLCs: 1
+-- Shared Depots: 3
+
+-- MAIN APPLICATION
+addappid(2119830) -- MISERY
+-- MAIN APP DEPOTS
+addappid(2119831, 1, "2322e54a5ba6afee939e05b81bdc7b5477adee6b3ef54dcab5d2ccc32243c086") -- Depot 2119831
+setManifestid(2119831, "8190048683933563036", 5887013774)
+-- SHARED DEPOTS (from other apps)
+addappid(228989, 1, "ad69276eb476cf06c40312df7376d63deac0c838b9a2767005be8bb306ffb853") -- VC 2022 Redist (Shared from App 228980)
+setManifestid(228989, "3514306556860204959", 39590283)
+addappid(228990, 1, "44d8c45ce229a11c4f231a3d2a350eaf80b0d69a8af938ec7ccca720f694b0e8") -- DirectX Jun 2010 Redist (Shared from App 228980)
+setManifestid(228990, "1829726630299308803", 102931551)
+addappid(229007, 1, "eba9fa9ff4f811929c119074450f49f739113bcae9317ded9bea262aa21ec58d") -- .NET 4.8 Redist (Shared from App 228980)
+setManifestid(229007, "4477590687906973371", 117381405)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(4119690) -- MISERY - Zaslavie Supporter Pack
