b8e634696eb20c38cd2916e7f4112f78b7742550
scloudsdb <steamcloudsdta@gmail.com>
2025-10-31 16:39:03 +0700

    Steamclouds Uploader (batch #1, 3 files) at 2025-10-31 16:39:02

---
+++ 1492730.lua
@@ -0,0 +1,24 @@
+-- 1492730's Lua and Manifest Created by Morrenus
+-- DORAEMON STORY OF SEASONS: Friends of the Great Kingdom
+-- Created: September 29, 2025 at 08:22:37 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 2
+-- Total DLCs: 7
+-- Shared Depots: 1
+
+-- MAIN APPLICATION
+addappid(1492730) -- DORAEMON STORY OF SEASONS: Friends of the Great Kingdom
+-- MAIN APP DEPOTS
+addappid(1492731, 1, "7a2c277cce45042f0a1d145eaf9c1d726882d47f61669b5f72ad56d991156276") -- Depot 1492731
+setManifestid(1492731, "6272594670443705729", 2106137318)
+-- SHARED DEPOTS (from other apps)
+addappid(228988, 1, "1845444d5e2cfd0ae65ae4a8fedb6e2fbf776fcc5b913ab4ac461bc9a74f8358") -- VC 2019 Redist (Shared from App 228980)
+setManifestid(228988, "6645201662696499616", 29212173)
+-- DLCS WITHOUT DEDICATED DEPOTS
+addappid(2019410) -- DORAEMON STORY OF SEASONS Friends of the Great Kingdom Summer Wear Set
+addappid(2062250) -- DORAEMON STORY OF SEASONS Friends of the Great Kingdom Premium Crops Seed Set
+addappid(2062251) -- DORAEMON STORY OF SEASONS Friends of the Great Kingdom Seasonal Veggies Seeds Set
+addappid(2062252) -- DORAEMON STORY OF SEASONS Friends of the Great Kingdom - Winter Tales
+addappid(2062253) -- DORAEMON STORY OF SEASONS Friends of the Great Kingdom - The Life of Insects
+addappid(2062254) -- DORAEMON STORY OF SEASONS Friends of the Great Kingdom - Together with Animals
+addappid(2062255) -- DORAEMON STORY OF SEASONS Friends of the Great Kingdom Season Pass
