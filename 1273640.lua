ffe610f29130b1191d44d64f2c6b721a3c803877
scloudsdb <steamcloudsdta@gmail.com>
2025-10-31 16:38:31 +0700

    Steamclouds Uploader (batch #1, 2 files) at 2025-10-31 16:38:30

---
+++ 1273640.lua
@@ -0,0 +1,12 @@
+-- 1273640's Lua and Manifest Created by Morrenus
+-- Bring You Home
+-- Created: October 19, 2025 at 04:59:38 EDT
+-- Website: https://manifest.morrenus.xyz/
+-- Total Depots: 1
+-- Total DLCs: 0
+
+-- MAIN APPLICATION
+addappid(1273640) -- Bring You Home
+-- MAIN APP DEPOTS
+addappid(1273643, 1, "fce3bc68b76c781b1084d70637da3afdc29bc96358e7e31a803210c64d694b9b") -- Depot 1273643
+setManifestid(1273643, "3557957694992901096", 2080074513)
