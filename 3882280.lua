b8ac7b75a40704016a3cc1365e0c8c14f94fd634
scloudsdb <steamcloudsdta@gmail.com>
2025-11-01 16:57:16 +0700

    Steamclouds Uploader (batch #1, 3 files) at 2025-11-01 16:57:16

---
+++ 3882280.lua
@@ -0,0 +1,7 @@
+addappid(3882280)
+addappid(3882281, 1, "2283b3e6922f6774d8e43d2ff476ca3fc6e9cbb505fb611a05509118f2b96721")
+setManifestid(3882281, "8537189057370696190", 173613207)
+--[[
+This file belongs to Ahmeds Bot Discord Server
+-----> join discord Server : https://discord.gg/sv6EGxCRnC
+]]
